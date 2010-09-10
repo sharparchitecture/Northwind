9b16d450a8b1b6f0a134b9adb50027950967c7f0
jongeorge1 <jon@jonathangeorge.co.uk>
2010-09-10 22:48:26 +0100

    Migrated Northwind app from core SA project.

---
+++ tests/Northwind.Tests/Northwind.Core/RegionTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Tests.Northwind.Core
+{
+    [TestFixture]
+    public class RegionTests
+    {
+        [Test]
+        public void CanCreateRegion() {
+            string regionDesc = "Northwest";
+            RegionWithPublicConstructor region = new RegionWithPublicConstructor(regionDesc);
+            Assert.That(region.Description, Is.EqualTo(regionDesc));
+        }
+    }
+}
