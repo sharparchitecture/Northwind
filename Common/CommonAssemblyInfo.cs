de6816d6e5708dbb24d4f2f569464cc7cab29c69
jongeorge1 <jon@jonathangeorge.co.uk>
2010-09-26 14:10:42 +0100

    Restructure repository to bring in line with others and allow new build system to be incorporated.

---
+++ Common/CommonAssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Sharp Architecture Development Team")]
+[assembly: AssemblyProduct("Sharp Architecture Northwind Sample")]
+[assembly: AssemblyCopyright("Copyright © Sharp Architecture Development Team 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
---
+++ Solutions/Northwind.Wcf/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Northwind.WcfServices")]
+[assembly: AssemblyDescription("")]
