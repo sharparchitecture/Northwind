fd944462f28056152886510897f15c1bb09172c6
Alec.Whittington <alec.whittington@gmail.com>
2010-10-28 20:34:36 -0700

    Added NHibernate.Validator Client side validation as provided by skirkland http://github.com/codai/Sharp-Architecture/pull/51

--- Solutions/Northwind.Web/Global.asax.cs
+++ Solutions/Northwind.Web/Global.asax.cs
@@ -1,84 +1,51 @@
-﻿namespace Northwind.Web
+﻿using System;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Castle.Windsor;
+using CommonServiceLocator.WindsorAdapter;
+using Microsoft.Practices.ServiceLocation;
+using MvcContrib.Castle;
+using Northwind.Data.NHibernateMaps;
+using Northwind.Web.CastleWindsor;
+using Northwind.Web.Controllers;
+using SharpArch.Core.NHibernateValidator.ValidatorProvider;
+using SharpArch.Data.NHibernate;
+using SharpArch.Web.Areas;
+using SharpArch.Web.ModelBinder;
+using SharpArch.Web.NHibernate;
+
+namespace Northwind.Web
 {
-    using System;
-    using System.Reflection;
-    using System.Web;
-    using System.Web.Mvc;
-    using System.Web.Routing;
-
-    using Castle.Windsor;
-
-    using CommonServiceLocator.WindsorAdapter;
-
-    using log4net.Config;
-
-    using Microsoft.Practices.ServiceLocation;
-
-    using MvcContrib.Castle;
-
-    using Northwind.Infrastructure.NHibernateMaps;
-    using Northwind.Web.CastleWindsor;
-    using Northwind.Web.Controllers;
-
-    using SharpArch.Core.NHibernateValidator.ValidatorProvider;
-    using SharpArch.Data.NHibernate;
-    using SharpArch.Web.Areas;
-    using SharpArch.Web.ModelBinder;
-    using SharpArch.Web.NHibernate;
-
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
+
     public class MvcApplication : HttpApplication
     {
-        private WebSessionStorage webSessionStorage;
-
-        public override void Init()
-        {
-            base.Init();
-
-            // The WebSessionStorage must be created during the Init() to tie in HttpApplication events
-            this.webSessionStorage = new WebSessionStorage(this);
-        }
-
-        /// <summary>
-        ///   Due to issues on IIS7, the NHibernate initialization cannot reside in Init() but
-        ///   must only be called once.  Consequently, we invoke a thread-safe singleton class to 
-        ///   ensure it's only initialized once.
-        /// </summary>
-        protected void Application_BeginRequest(object sender, EventArgs e)
-        {
-            NHibernateInitializer.Instance().InitializeNHibernateOnce(() => this.InitializeNHibernateSession());
-        }
-
-        protected void Application_Error(object sender, EventArgs e)
-        {
-            // Useful for debugging
-            var ex = this.Server.GetLastError();
-            var reflectionTypeLoadException = ex as ReflectionTypeLoadException;
-        }
-
         protected void Application_Start()
         {
-            XmlConfigurator.Configure();
+            log4net.Config.XmlConfigurator.Configure();
 
             ViewEngines.Engines.Clear();
             ViewEngines.Engines.Add(new AreaViewEngine());
 
             DataAnnotationsModelValidatorProvider.AddImplicitRequiredAttributeForValueTypes = false;
-            ModelValidatorProviders.Providers.Add(new NHibernateValidatorProvider()); // Server side validation provider
+            ModelValidatorProviders.Providers.Add(new NHibernateValidatorProvider()); //Server side validation provider
+            ModelValidatorProviders.Providers.Add(new NHibernateValidatorClientProvider()); //Client side validation provider
 
             ModelBinders.Binders.DefaultBinder = new SharpModelBinder();
 
-            var container = this.InitializeServiceLocator();
+            IWindsorContainer container = InitializeServiceLocator();
 
             AreaRegistration.RegisterAllAreas();
             RouteRegistrar.RegisterRoutesTo(RouteTable.Routes);
         }
 
         /// <summary>
-        ///   Instantiate the container and add all Controllers that derive from 
-        ///   WindsorController to the container.  Also associate the Controller 
-        ///   with the WindsorContainer ControllerFactory.
+        /// Instantiate the container and add all Controllers that derive from 
+        /// WindsorController to the container.  Also associate the Controller 
+        /// with the WindsorContainer ControllerFactory.
         /// </summary>
         protected virtual IWindsorContainer InitializeServiceLocator()
         {
@@ -93,18 +60,47 @@
             return container;
         }
 
+        public override void Init()
+        {
+            base.Init();
+
+            // The WebSessionStorage must be created during the Init() to tie in HttpApplication events
+            webSessionStorage = new WebSessionStorage(this);
+        }
+
         /// <summary>
-        ///   If you need to communicate to multiple databases, you'd add a line to this method to
-        ///   initialize the other database as well.
+        /// Due to issues on IIS7, the NHibernate initialization cannot reside in Init() but
+        /// must only be called once.  Consequently, we invoke a thread-safe singleton class to 
+        /// ensure it's only initialized once.
+        /// </summary>
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            NHibernateInitializer.Instance().InitializeNHibernateOnce(
+                () => InitializeNHibernateSession());
+        }
+
+        /// <summary>
+        /// If you need to communicate to multiple databases, you'd add a line to this method to
+        /// initialize the other database as well.
         /// </summary>
         private void InitializeNHibernateSession()
         {
-            NHibernateSession.ConfigurationCache = new NHibernateConfigurationFileCache(new[] { "Northwind.Core" });
+            NHibernateSession.ConfigurationCache = new NHibernateConfigurationFileCache(
+                new string[] { "Northwind.Core" });
             NHibernateSession.Init(
-                this.webSessionStorage, 
-                new[] { this.Server.MapPath("~/bin/Northwind.Infrastructure.dll") }, 
-                new AutoPersistenceModelGenerator().Generate(), 
-                this.Server.MapPath("~/NHibernate.config"));
+                webSessionStorage,
+                new string[] { Server.MapPath("~/bin/Northwind.Data.dll") },
+                new AutoPersistenceModelGenerator().Generate(),
+                Server.MapPath("~/NHibernate.config"));
         }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            // Useful for debugging
+            Exception ex = Server.GetLastError();
+            ReflectionTypeLoadException reflectionTypeLoadException = ex as ReflectionTypeLoadException;
+        }
+
+        private WebSessionStorage webSessionStorage;
     }
 }
--- Solutions/Northwind.Web/Views/Organization/Employees/EmployeeForm.ascx
+++ Solutions/Northwind.Web/Views/Organization/Employees/EmployeeForm.ascx
@@ -1,24 +1,19 @@
 <%@ Control Language="C#" AutoEventWireup="true"
 	Inherits="System.Web.Mvc.ViewUserControl<Northwind.Web.Controllers.Organization.EmployeesController.EmployeeFormViewModel>" %>
 <%@ Import Namespace="Northwind.Web.Controllers" %>
+<%@ Import Namespace="Northwind.Core" %>
 <%@ Import Namespace="Northwind.Web.Controllers.Organization" %>
 
-<%
-    if (this.ViewContext.TempData[ControllerEnums.GlobalViewDataProperty.PageMessage.ToString()] != null)
-    {%>
-    <p id="pageMessage"><%=this.ViewContext.TempData[ControllerEnums.GlobalViewDataProperty.PageMessage.ToString()]%></p>
-<%
-    }%>
+<% if (ViewContext.TempData[ControllerEnums.GlobalViewDataProperty.PageMessage.ToString()] != null) { %>
+    <p id="pageMessage"><%= ViewContext.TempData[ControllerEnums.GlobalViewDataProperty.PageMessage.ToString()]%></p>
+<% } %>
 
-<%=this.Html.ValidationSummary()%>
+<% Html.EnableClientValidation(); %>
+<%= Html.ValidationSummary() %>
 
-<%
-    using (this.Html.BeginForm())
-    {%>
-    <%=this.Html.AntiForgeryToken()%>
-    <%=
-            this.Html.Hidden(
-                "Employee.Id", (this.ViewData.Model.Employee != null) ? this.ViewData.Model.Employee.Id : 0)%>
+<% using (Html.BeginForm()) { %>
+    <%= Html.AntiForgeryToken() %>
+    <%= Html.Hidden("Employee.Id", (ViewData.Model.Employee != null) ? ViewData.Model.Employee.Id : 0)%>
 
     <!--
         Be sure to use CSS driven forms instead of layout via tables; 
@@ -27,76 +22,58 @@
     -->
     <ul>
 		<li>
-			<label for="Employee_FirstName">FirstName:</label>
+		    <%= Html.LabelFor(x=>x.Employee.FirstName) %>
 			<div>
-				<%=
-            this.Html.TextBox(
-                "Employee.FirstName",
-                (this.ViewData.Model.Employee != null) ? this.ViewData.Model.Employee.FirstName : "")%>
+				<%= Html.EditorFor(x=>x.Employee.FirstName) %>
 			</div>
-			<%=this.Html.ValidationMessage("Employee.FirstName")%>
+			<%= Html.ValidationMessageFor(x=>x.Employee.FirstName)%>
 		</li>
 		<li>
-			<label for="Employee_LastName">LastName:</label>
+			<%= Html.LabelFor(x=>x.Employee.LastName) %>
 			<div>
-				<%=
-            this.Html.TextBox(
-                "Employee.LastName", (this.ViewData.Model.Employee != null) ? this.ViewData.Model.Employee.LastName : "")%>
+			    <%= Html.TextBoxFor(x=>x.Employee.LastName) %>
 			</div>
-			<%=this.Html.ValidationMessage("Employee.LastName")%>
+			<%= Html.ValidationMessageFor(x=>x.Employee.LastName) %>
 		</li>
 		<li>
-			<label for="Employee_PhoneExtension">PhoneExtension:</label>
+		    <%= Html.LabelFor(x=>x.Employee.PhoneExtension) %>
 			<div>
-				<%=
-            this.Html.TextBox(
-                "Employee.PhoneExtension",
-                (this.ViewData.Model.Employee != null) ? this.ViewData.Model.Employee.PhoneExtension.ToString() : "")%>
+			    <%= Html.EditorFor(x=>x.Employee.PhoneExtension) %>
 			</div>
-			<%=this.Html.ValidationMessage("Employee.PhoneExtension")%>
+			<%= Html.ValidationMessageFor(x=>x.Employee.PhoneExtension) %>
 		</li>
 		<li>
 			<label for="Employee_Territories">Territories:</label>
             <span id="Employee_Territories">
                 <table>
                     <tr>
-                    <%
-        for (var i = 0; i < this.ViewData.Model.AvailableTerritories.Count(); i++)
-        {
-            if (i > 0 && i % 4 == 0)
-            {
-                %></tr><tr><%
-            }
+                    <% for (int i = 0; i < ViewData.Model.AvailableTerritories.Count(); i++) {
+                        if (i > 0 && i % 4 == 0) {
+                           %></tr><tr><%
+                        }
 
-            var territory = this.ViewData.Model.AvailableTerritories[i];
-%>
+                        Territory territory = ViewData.Model.AvailableTerritories[i];
+                        %>
                         <td>
                             <!-- 
                             It's very important that the name of the checkbox has a "." in it so that 
                             the binder sees it as a property value.
                             -->
-                            <input type="checkbox" name="Employee.Territories" value="<%=territory.Id%>" <%
-            if (this.ViewData.Model.Employee != null && this.ViewData.Model.Employee.Territories.Contains(territory))
-            {%>checked="checked"<%
-            }%>/> 
-                            <%=territory.Description%>
+                            <input type="checkbox" name="Employee.Territories" value="<%= territory.Id %>" <%
+                                if (ViewData.Model.Employee != null && ViewData.Model.Employee.Territories.Contains(territory)) { 
+                                    %>checked="checked"<% 
+                                } %>/> 
+                            <%= territory.Description%>
                         </td>
-                    <%
-        }%>
+                    <% } %>
                     </tr>
                 </table>
             </span>
 		</li>
 	    <li>
-            <%=this.Html.SubmitButton("btnSave", "Save Employee")%>
-	        <%=
-            this.Html.Button(
-                "btnCancel",
-                "Cancel",
-                HtmlButtonType.Button,
-                "window.location.href = '" + this.Html.BuildUrlFromExpression<EmployeesController>(c => c.Index()) +
-                "';")%>
+            <%= Html.SubmitButton("btnSave", "Save Employee") %>
+	        <%= Html.Button("btnCancel", "Cancel", HtmlButtonType.Button, 
+				    "window.location.href = '" + Html.BuildUrlFromExpression<EmployeesController>(c => c.Index()) + "';") %>
         </li>
     </ul>
-<%
-    }%>
+<% } %>
