744f8937afeb6e76212d658eaef98b8673361c95
Seif Attar <iam@seifattar.net>
2011-10-16 14:49:55 +0100

    Add ServiceLocatorHelper from WCHM

---
+++ Solutions/Northwind.Tests/ServiceLocatorHelper.cs
@@ -0,0 +1,30 @@
+﻿namespace Tests
+{
+    using Microsoft.Practices.ServiceLocation;
+
+    using Rhino.Mocks;
+
+    public static class ServiceLocatorHelper
+    {
+        private static IServiceLocator provider;
+
+        public static void InitialiseServiceLocator()
+        {
+            provider = MockRepository.GenerateStub<IServiceLocator>();
+
+            ServiceLocator.SetLocatorProvider(() => provider);
+        }
+
+        public static T AddToServiceLocator<T>(this T o)
+        {
+            if (provider == null)
+            {
+                InitialiseServiceLocator();
+            }
+
+            provider.Stub(p => p.GetInstance<T>()).Return(o);
+            return o;
+        }
+    }
+}
+
